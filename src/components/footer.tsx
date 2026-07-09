71c882a62ba0a819a3782e3d3e59b14cae8ff033
haenlau <102852113+haenlau@users.noreply.github.com>
2026-07-09 19:53:50 +0800

    Update footer copy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,3 @@
 export function Footer() {
-  return (
-    <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
-      <span>
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
-        </a>
-      </span>
-    </>
-  )
+  return <>Part of Air1 Quick Tools · Powered by Cloudflare</>
 }
